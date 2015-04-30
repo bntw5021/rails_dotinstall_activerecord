592bf7fd6ab608ddf8a569fda652ee9a792dcb85
bntw5021 <watanabe1205@gmail.com>
2015-04-30 22:29:06 +0900

    #08

--- main.rb
+++ main.rb
@@ -1,21 +1,20 @@
 require 'active_record'
-require 'logger'
 
 ActiveRecord::Base.establish_connection(
     "adapter" => "sqlite3",
     "database" => "./blog.db"
 )
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
-
 class Post < ActiveRecord::Base
-    scope :top3, -> { order("created_at").limit(3) }
 end
 
-#p Post.where(:id => 1..3)
-#p Post.where(:id => [1, 3])
+#Post.where(:title => "title5").first_or_create
 
-#p Post.order("id desc").limit(3)
+Post.where(:title => "title6").first_or_create do |p|
+    p.body = "hello6"
+end
 
-p Post.top3
+p Post.all
+
+
 
