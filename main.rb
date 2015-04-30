cc792845fbced7399514d3b10cf2c878a2863b92
bntw5021 <watanabe1205@gmail.com>
2015-04-30 22:06:06 +0900

    #03

---
+++ main.rb
@@ -0,0 +1,15 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    "adapter" => "sqlite3",
+    "database" => "./blog.db"
+)
+
+class Post < ActiveRecord::Base
+end
+
+post = Post.new(:title => "title1", :body => "hello1")
+post.save
+
+p Post.all
+
