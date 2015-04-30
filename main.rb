1832234361a823a9d6652056ac0eb4c06f236ce4
bntw5021 <watanabe1205@gmail.com>
2015-04-30 22:47:20 +0900

    #12

--- main.rb
+++ main.rb
@@ -6,15 +6,6 @@ ActiveRecord::Base.establish_connection(
 )
 
 class Post < ActiveRecord::Base
-    validates :title, :presence => true
-    validates :body, :length => { :minimum => 5 }
 end
 
-post = Post.new(:body => "123")
-
-if !post.save
-    p post.errors.messages
-end
-
-p Post.all
 
